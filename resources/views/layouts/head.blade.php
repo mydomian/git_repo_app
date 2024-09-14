b1ec73975f94a0ef1417b9bf236a51802903c823
mydomian <mortuza504@gmail.com>
2024-09-14 16:59:06 +0600

    Project Submition Done

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,9 +3,9 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 
 <title>Git Profile & Repos</title>
-<link rel="stylesheet" href="{{ asset('/plugins/fontawesome-free/css/all.min.css') }}">
-<link rel="stylesheet" href="{{ asset('/dist/css/adminlte.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/storage/plugins/fontawesome-free/css/all.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/storage/dist/css/adminlte.min.css') }}">
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
-<link rel="stylesheet" href="{{ asset('/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
-    <link rel="stylesheet" href="{{ asset('/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+<link rel="stylesheet" href="{{ asset('/storage/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('/storage/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
 @stack('css-links')
--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -1,10 +1,10 @@
-<script src="{{ asset('/plugins/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('/dist/js/adminlte.min.js') }}"></script>
-<script src="{{ asset('/plugins/datatables/jquery.dataTables.min.js') }}"></script>
-    <script src="{{ asset('/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
-    <script src="{{ asset('/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
-    <script src="{{ asset('/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ asset('/storage/plugins/jquery/jquery.min.js') }}"></script>
+<script src="{{ asset('/storage/plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+<script src="{{ asset('/storage/dist/js/adminlte.min.js') }}"></script>
+<script src="{{ asset('/storage/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('/storage/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+    <script src="{{ asset('/storage/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('/storage/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
 
     <script>
         $(function () {
